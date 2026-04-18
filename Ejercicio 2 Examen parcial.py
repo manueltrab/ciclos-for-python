e942e4a3e4c2e53c2f952d42b32fd7ec30ed5e5c
manueltrab <manueltrab@gmail.com>
2026-04-18 10:59:39 -0600

    Create Ejercicio 2 Examen parcial.py

---
+++ Ejercicio 2 Examen parcial.py
@@ -0,0 +1,10 @@
+#seudocodigo
+#import reques
+#consumir url https://pokeapi.co/api/v2/pokemon?limit=20
+#validar con if si la peticion fue exitosa (200)
+#Lógica y Condicional: Dentro del ciclo, para cada pokemón, deberás imprimir su nombre. Sin embargo, hay una regla especial:
+#Si el nombre del pokemón comienza con la letra 'b' (mayúscula o minúscula), debes imprimir: [ESPECIAL] NombreDelPokemon.
+#De lo contrario, simplemente imprime: Nombre: NombreDelPokemon.
+
+
+
